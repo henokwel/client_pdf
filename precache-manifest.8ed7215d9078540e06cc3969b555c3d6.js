b8470ad03f76bf93f5ff702f6b57044b00a0024f
henWel <henokwel@outlook.com>
2020-10-05 23:50:50 +0200

    Updates

---
+++ precache-manifest.8ed7215d9078540e06cc3969b555c3d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d866a8762ff0f6367be23ba85c2d3bec",
+    "url": "/client_pdf/index.html"
+  },
+  {
+    "revision": "066cb19100fe47bf0e51",
+    "url": "/client_pdf/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6e904aaca39cd95b4cae",
+    "url": "/client_pdf/static/js/2.f5011422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/client_pdf/static/js/2.f5011422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "066cb19100fe47bf0e51",
+    "url": "/client_pdf/static/js/main.35708675.chunk.js"
+  },
+  {
+    "revision": "48ebc4fd8e5a0d741633",
+    "url": "/client_pdf/static/js/runtime-main.c9692816.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/client_pdf/static/media/logo.5d5d9eef.svg"
+  }
+]);
